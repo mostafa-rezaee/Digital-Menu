0bf74b50923e04555613cb9ea69db04297b44065
mostafa rezaie <mostafa.rezaie@gmail.com>
2024-08-19 17:17:29 +0330

    Add project files.

---
+++ DigiMenu.Dommain/Entities/Users/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiMenu.Domain.Entities.Users
+{
+    public class User
+    {
+        public User(string name, string username, string password, bool isAdmin)
+        {
+            Name = name;
+            Username = username;
+            Password = password;
+            IsAdmin = isAdmin;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; } = string.Empty;
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public bool IsAdmin { get; private set; } = false;
+
+    }
+}
