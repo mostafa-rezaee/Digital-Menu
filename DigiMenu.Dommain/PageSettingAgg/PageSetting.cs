a2b496db9dfb3cb87029dc435b65220d322cf7e7
mostafa rezaie <mostafa.rezaie@gmail.com>
2024-08-20 16:06:22 +0330

    Added PageSetting's Aggregate & Repository

---
+++ DigiMenu.Dommain/PageSettingAgg/PageSetting.cs
@@ -0,0 +1,49 @@
+﻿using Common.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiMenu.Domain.PageSettingAgg
+{
+    public class PageSetting : AggregateRoot
+    {
+        public PageSetting(string pageTitle, string bGImageName, string logoImageName, string websiteAddress, 
+                string socialTitle, string socialAddress, string telephone, string address)
+        {
+            PageTitle = pageTitle;
+            BGImageName = bGImageName;
+            LogoImageName = logoImageName;
+            WebsiteAddress = websiteAddress;
+            SocialTitle = socialTitle;
+            SocialAddress = socialAddress;
+            Telephone = telephone;
+            Address = address;
+        }
+
+        public string PageTitle { get; private set; }
+        public string BGImageName { get; private set; }
+        public string LogoImageName{ get; private set; }
+        public string WebsiteAddress { get; private set; }
+        public string SocialTitle { get; private set; }
+        public string SocialAddress { get; private set; }
+        public string Telephone { get; private set; }
+        public string Address { get; private set; }
+
+        public void Edit(string pageTitle, string bGImageName, string logoImageName, string websiteAddress,
+                string socialTitle, string socialAddress, string telephone, string address)
+        {
+            PageTitle = pageTitle;
+            BGImageName = bGImageName;
+            LogoImageName = logoImageName;
+            WebsiteAddress = websiteAddress;
+            SocialTitle = socialTitle;
+            SocialAddress = socialAddress;
+            Telephone = telephone;
+            Address = address;
+        }
+
+
+    }
+}
